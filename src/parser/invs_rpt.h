926df433ff89a6e218c71cc12aaf070c03b3ff4e
kinling9 <vmin6810@gmail.com>
2024-09-12 15:06:26 +0800

    remove unused rpts

--- src/parser/invs_rpt.h
+++ src/parser/invs_rpt.h
@@ -20,5 +20,4 @@ class invs_rpt_parser : public rpt_parser {
   const RE2 _group_pattern{"^Path Groups: {(\\S*)}"};
   const RE2 _clock_pattern{"triggered by\\s+leading edge of\\s+'(.*)'"};
   const RE2 _slack_pattern{"^= Slack Time\\s+([0-9\\.-]*).*"};
-  const RE2 _with_net_pattern{"^.*Net.*"};
 };
