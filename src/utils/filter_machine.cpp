af8de60141c95152cc829e2d92d4034a5e0c88b7
kinling9 <vmin6810@gmail.com>
2024-09-23 16:46:59 +0800

    add filter_mechine

---
+++ src/utils/filter_machine.cpp
@@ -0,0 +1,65 @@
+#include "filter_machine.h"
+
+std::pair<double, double> filter_machine::pop2() {
+  double a = _stack.top();
+  _stack.pop();
+  double b = _stack.top();
+  _stack.pop();
+  return std::make_pair(a, b);
+}
+
+bool filter_machine::execute(const std::vector<double> &code) {
+  std::vector<double>::const_iterator pc = code.begin();
+  while (pc != code.end()) {
+    switch (static_cast<int>(*pc++)) {
+      case op_l: {
+        auto [rhs, lhs] = pop2();
+        _stack.push(lhs < rhs);
+        break;
+      }
+      case op_le: {
+        auto [rhs, lhs] = pop2();
+        _stack.push(lhs <= rhs);
+        break;
+      }
+      case op_g: {
+        auto [rhs, lhs] = pop2();
+        _stack.push(lhs > rhs);
+        break;
+      }
+      case op_ge: {
+        auto [rhs, lhs] = pop2();
+        _stack.push(lhs >= rhs);
+        break;
+      }
+      case op_eq: {
+        auto [rhs, lhs] = pop2();
+        _stack.push(lhs == rhs);
+        break;
+      }
+      case op_and: {
+        auto [rhs, lhs] = pop2();
+        _stack.push(lhs && rhs);
+        break;
+      }
+      case op_or: {
+        auto [rhs, lhs] = pop2();
+        _stack.push(lhs || rhs);
+        break;
+      }
+      case op_not: {
+        double a = _stack.top();
+        _stack.pop();
+        _stack.push(!a);
+        break;
+      }
+      case op_int:
+        _stack.push(*pc++);
+        break;
+      case op_x:
+        _stack.push(_x);
+        break;
+    }
+  }
+  return static_cast<bool>(_stack.top());
+}
