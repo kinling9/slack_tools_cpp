cb6d480fd202ceec467030bf1487bf3f4bf5a6fb
kinling9 <vmin6810@gmail.com>
2024-09-27 18:26:30 +0800

    support cell arc analyse

--- src/analyser/arc_analyser.cpp
+++ src/analyser/arc_analyser.cpp
@@ -44,25 +44,32 @@ void arc_analyser::match(
     const std::string &design,
     const absl::flat_hash_map<std::string_view, std::shared_ptr<Path>> &pin_map,
     const std::vector<std::shared_ptr<basedb>> &dbs) {
-  auto fanout_filter = [&](const std::shared_ptr<Pin> pin_ptr) {
-    if (_configs.slack_filter_op_code.empty()) {
-      return true;
-    }
-    return pin_ptr->is_input &&
-           double_filter(_configs.slack_filter_op_code, pin_ptr->net->fanout);
-  };
-  auto delay_filter = [&](const std::shared_ptr<Pin> pin_ptr) {
-    if (_configs.delay_filter_op_code.empty()) {
-      return true;
-    }
-    return double_filter(_configs.delay_filter_op_code, pin_ptr->incr_delay);
-  };
+  auto fanout_filter =
+      [&](const std::tuple<std::shared_ptr<Pin>, std::shared_ptr<Pin>>
+              pin_ptr_tuple) {
+        if (_configs.fanout_filter_op_code.empty()) {
+          return true;
+        }
+        const auto &[pin_ptr, _] = pin_ptr_tuple;
+        return !pin_ptr->is_input &&
+               double_filter(_configs.fanout_filter_op_code,
+                             pin_ptr->net->fanout);
+      };
+  auto delay_filter =
+      [&](const std::tuple<std::shared_ptr<Pin>, std::shared_ptr<Pin>>
+              pin_ptr_tuple) {
+        if (_configs.delay_filter_op_code.empty()) {
+          return true;
+        }
+        const auto &[pin_ptr, _] = pin_ptr_tuple;
+        return double_filter(_configs.delay_filter_op_code,
+                             pin_ptr->incr_delay);
+      };
   for (const auto &path : dbs[0]->paths) {
-    for (const auto &pin : path->path | std::views::filter(delay_filter) |
-                               std::views::filter(fanout_filter)) {
-      auto net = pin->net;
-      const auto &pin_from = net->pins.first;
-      const auto &pin_to = net->pins.second;
+    for (const auto &pin_tuple : path->path | std::views::adjacent<2> |
+                                     std::views::filter(delay_filter) |
+                                     std::views::filter(fanout_filter)) {
+      const auto &[pin_from, pin_to] = pin_tuple;
       if (pin_map.contains(pin_from->name) && pin_map.contains(pin_to->name)) {
         if (pin_map.at(pin_from->name) == pin_map.at(pin_to->name) &&
             !_arcs_buffer.contains({pin_from->name, pin_to->name})) {
@@ -79,7 +86,7 @@ void arc_analyser::match(
                          "pin_name: {}, incr_delay: {}, path_delay: {}\n",
                          pin_to->name, pin_to->incr_delay, pin_to->path_delay);
           fmt::format_to(std::back_inserter(buffer), "In value file:\n");
-          auto &value_path = pin_map.at(net->pins.first->name);
+          auto &value_path = pin_map.at(pin_from->name);
           bool match = true;
           double key_delay = pin_to->incr_delay;
           double value_delay = 0;
@@ -87,21 +94,21 @@ void arc_analyser::match(
                value_path->path |
                    std::views::drop_while(
                        [&](const std::shared_ptr<Pin> from_pin) {
-                         return from_pin->name != net->pins.first->name;
+                         return from_pin->name != pin_from->name;
                        }) |
                    std::views::take_while(
                        [&](const std::shared_ptr<Pin> to_pin) {
-                         if (to_pin->name == net->pins.second->name) {
+                         if (to_pin->name == pin_to->name) {
                            match = false;
                            return true;
                          }
-                         return match || to_pin->name == net->pins.second->name;
+                         return match || to_pin->name == pin_to->name;
                        })) {
             fmt::format_to(std::back_inserter(buffer),
                            "pin_name: {}, incr_delay: {}, path_delay: {}\n",
                            value_pin->name, value_pin->incr_delay,
                            value_pin->path_delay);
-            if (value_pin->name == net->pins.first->name) {
+            if (value_pin->name == pin_from->name) {
               continue;
             }
             value_delay += value_pin->incr_delay;
@@ -112,7 +119,6 @@ void arc_analyser::match(
                          key_delay, value_delay, delta_delay);
           _arcs_delta[std::make_pair(pin_from->name, pin_to->name)] =
               delta_delay;
-          buffer.push_back('\0');
           _arcs_buffer[std::make_pair(pin_from->name, pin_to->name)] =
               fmt::to_string(buffer);
         }
