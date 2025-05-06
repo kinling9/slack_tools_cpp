892c6c89e2210d32465b02085142fe04e6f056bf
kinling9 <vmin6810@gmail.com>
2025-05-06 11:17:09 +0800

    beautify the code

--- python/plot_nd_buffer.py
+++ python/plot_nd_buffer.py
@@ -3,15 +3,28 @@
 
 import json
 import argparse
-import csv
 import matplotlib.pyplot as plt
+
+
+def calculate_buffer_num(data_item):
+    """Calculate buffer number based on pins length."""
+    return (len(data_item["value"]["pins"]) - 3) / 2
+
+
+def calculate_delta_delay(data_item):
+    """Calculate delta delay between key and value pins."""
+    key_pin_delays = sum(pin["incr_delay"] for pin in data_item["key"]["pins"][1:-2])
+    value_pin_delays = sum(
+        pin["incr_delay"] for pin in data_item["value"]["pins"][1:-2]
+    )
+    return key_pin_delays - value_pin_delays
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
         description="Process delay data from JSON to plot."
     )
     parser.add_argument("input_json", type=str, help="Input JSON file")
-
     args = parser.parse_args()
 
     input_json = args.input_json
@@ -19,13 +32,8 @@ if __name__ == "__main__":
     with open(input_json, "r") as file:
         data = json.load(file)
 
-    # Example: Accessing values (adjust according to your JSON structure)
-    buffer_num = [(len(item["value"]["pins"]) - 3) / 2 for _, item in data.items()]
-    delta_delay = [
-        sum([i["incr_delay"] for i in item["key"]["pins"][1:-2]])
-        - sum([i["incr_delay"] for i in item["value"]["pins"][1:-2]])
-        for _, item in data.items()
-    ]
+    buffer_num = [calculate_buffer_num(item) for _, item in data.items()]
+    delta_delay = [calculate_delta_delay(item) for _, item in data.items()]
 
     plt.figure(figsize=(8, 6))
     plt.scatter(buffer_num, delta_delay, color="blue", label="Delta Delay")
